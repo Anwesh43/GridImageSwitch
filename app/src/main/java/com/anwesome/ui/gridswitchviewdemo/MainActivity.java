10cd07bceeb25bf296f71338517625e4c8680b3e
Anwesh43 <anweshthecool0@gmail.com>
2017-04-22 16:01:58 +0530

    Using GridSwitch in app

--- app/src/main/java/com/anwesome/ui/gridswitchviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/gridswitchviewdemo/MainActivity.java
@@ -4,8 +4,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.anwesome.ui.gridswitch.GridSwitch;
 import com.anwesome.ui.gridswitch.GridSwitchView;
+import com.anwesome.ui.gridswitch.OnSelectionListener;
 
 public class MainActivity extends AppCompatActivity {
     private int resources[] = {R.drawable.bluetooth,R.drawable.data,R.drawable.sync,R.drawable.wifi};
@@ -16,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<resources.length;i++) {
             bitmaps[i] = BitmapFactory.decodeResource(getResources(),resources[i]);
         }
-        GridSwitchView gridSwitchView = new GridSwitchView(this);
-        for(int i=0;i<4;i++) {
-            gridSwitchView.addGridElement(bitmaps[i]);
+        GridSwitch gridSwitch = new GridSwitch(this);
+        for(int i=0;i<bitmaps.length*10;i++) {
+            final int index = i+1;
+            gridSwitch.addGridElement(bitmaps[i % bitmaps.length], new OnSelectionListener() {
+                @Override
+                public void onSelect() {
+                    Toast.makeText(MainActivity.this, "Toast number "+index+" selected", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onUnSelect() {
+                    Toast.makeText(MainActivity.this, "Toast number "+index+" unselected", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
-        setContentView(gridSwitchView);
+        gridSwitch.show();
     }
 }
