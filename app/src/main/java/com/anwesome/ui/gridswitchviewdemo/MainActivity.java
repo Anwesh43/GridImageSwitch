2d356758d3278b878b4c22dd4e8b8021cb816706
Anwesh43 <anweshthecool0@gmail.com>
2017-04-22 13:08:49 +0530

    Using GridSwitchView in app

--- app/src/main/java/com/anwesome/ui/gridswitchviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/gridswitchviewdemo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.anwesome.ui.gridswitchviewdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.gridswitch.GridSwitchView;
 
+public class MainActivity extends AppCompatActivity {
+    private int resources[] = {R.drawable.bluetooth,R.drawable.data,R.drawable.sync,R.drawable.wifi};
+    private Bitmap bitmaps[] = new Bitmap[4];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        for(int i=0;i<resources.length;i++) {
+            bitmaps[i] = BitmapFactory.decodeResource(getResources(),resources[i]);
+        }
+        GridSwitchView gridSwitchView = new GridSwitchView(this);
+        for(int i=0;i<4;i++) {
+            gridSwitchView.addGridElement(bitmaps[i]);
+        }
+        setContentView(gridSwitchView);
     }
 }
